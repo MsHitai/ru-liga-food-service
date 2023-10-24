8645722b7dd6c6fdc17210118df74a6e8d49e1c6
MsHitai <mmalafeeva@yandex.ru>
2023-10-24 21:07:35 +0300

    test github actions

--- common/src/main/java/ru/liga/clients/KitchenClient.java
+++ common/src/main/java/ru/liga/clients/KitchenClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.liga.dto.OrderActionDto;
 
-@FeignClient(name = "kitchen-client", url = "http://localhost:8081")
+@FeignClient(name = "kitchen-client", url = "${delivery.service.url}")
 public interface KitchenClient {
 
     @PostMapping(value = "/deliveries")
